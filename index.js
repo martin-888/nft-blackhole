c5ae52c230da95e412cd73b970cdbb7ffcb7898a
Martin Babinec <M@rtinBabinec.cz>
2021-10-25 23:33:08 -0500

    Working app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(404).send("");
+})
+
+app.get('/nft/:id', (req, res) => {
+  res.json({
+    name: "",
+    image: "https://gateway.pinata.cloud/ipfs/QmNQq33D3Y1LhftVkHbVJZziuFLQuNLvFmSExwtEcKXcJx",
+    attributes: []
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
